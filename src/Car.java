0db53f19d6b420a64f419a5143c50672be34ecfe
ttzawadzki33600 <s33600@pjwstk.edu.pl>
2025-05-27 18:31:56 +0200

    init commit

--- src/Car.java
+++ src/Car.java
@@ -2,11 +2,24 @@ public class Car extends Vehicle{
     private int numberOfSeats;
 
     public Car(int numberOfSeats){
+
         this.numberOfSeats = numberOfSeats;
     }
 
     public int getNumberOfSeats(){
+
         return numberOfSeats;
+    }
+
+    @Override
+    public void start(){
+        System.out.println("Samochód został uruchomiony.");
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("Samochód został zatrzymany.");
+
     }
 
 
